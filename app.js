dd7681533087c9b6818a823aa0f5e9c0aab56039
Sarad3993 <risingstar3993@gmail.com>
2023-01-23 22:55:19 +0545

    connection to mongodb database

--- app.js
+++ app.js
@@ -3,32 +3,35 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 
-const notFound = require("./middlewares/not-found");
-const errorHandler = require('./middlewares/error-handler')
+const connectDB = require('./db/connect')
+
+const notFoundMiddleware = require("./middlewares/not-found");
+const errorHandlerMiddleware = require('./middlewares/error-handler');
 
 //middlewares 
 app.use(express.json())
 
 
 //routes 
-
 app.get('/', (req,res)=>{
     res.send('Hello this is store api')
 })
 
-app.use(notFound)
-app.use(errorHandler)
+app.use(notFoundMiddleware)
+app.use(errorHandlerMiddleware)
+
+
 
 port = process.env.PORT || 3000
 
 const start = async () =>{
     try {
-        // connect to db 
+        await connectDB(process.env.MONGO_URI)
         app.listen(port, console.log(`Server connected to port ${port}..`))
     } catch (error) {
+        console.log(error);
         
     }
 }
-
 
 start()
