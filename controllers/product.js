234f73c077760b9045970e7f83ac247fbe66f33f
Sarad3993 <risingstar3993@gmail.com>
2023-01-28 18:07:11 +0545

    pagination

--- controllers/product.js
+++ controllers/product.js
@@ -1,20 +1,56 @@
-const Product = require('../models/product')
+const Product = require("../models/product");
 
-const getAllProductsStatic = async (req,res) =>{
-    const products = await Product.find({'company':'marcos'})
-    res.status(200).json({products, nOfItems:products.length})
-}
+// this is for testing purpose 
+const getAllProductsStatic = async (req, res) => {
+  const products = await Product.find({})
+  .sort('name')
+  .select('name price')
+  .limit(10)
+  .skip(5)
+  res.status(200).json({ products, nOfItems: products.length });
+};
 
+// this is actual
 const getAllProducts = async (req, res) => {
-  const {featured} = req.query
-  const queryObject = {}
-  if(featured){
-    queryObject.featured = featured === 'true'? true : false 
+  const { featured, company, name, sort, selectFields } = req.query;
+  const queryObject = {};
+  if (featured) {
+    queryObject.featured = featured === "true" ? true : false;
+  }
+  // if company exists set my property on query object
+  if (company) {
+    queryObject.company = company;
+  }
+
+  if(name){
+    queryObject.name = { $regex: name, $options: "i" };
   }
   // console.log(queryObject);
-  const products = await Product.find(queryObject);
+  //sort
+  let result = Product.find(queryObject);
+  if(sort){
+    const sortList = sort.split(',').join(' ');
+    result = result.sort(sortList)
+  }
+  else{
+    result = result.sort('createdAt')
+  }
+  if (selectFields){
+    const fieldsList = selectFields.split(',').join(' ')
+    result = result.select(fieldsList)
+  }
+
+//pagination
+  const page = Number(req.query.page) || 1
+  const limit = Number(req.query.limit) || 10
+  const skip = (page -1) * limit;
+  
+  result = result.skip(skip).limit(limit) 
+
+
+  const products = await result 
   res.status(200).json({ products, nOfItems: products.length });
 };
 
 
-module.exports = {getAllProductsStatic, getAllProducts}
+module.exports = { getAllProductsStatic, getAllProducts };
